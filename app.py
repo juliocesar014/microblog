d463c4af718895fab9a832e920b59b6d07773661
juliocesar014 <juliocesar014@live.com>
2022-12-28 18:14:05 -0300

    fix/feat resolving problem db

--- app.py
+++ app.py
@@ -4,19 +4,30 @@ from db import db
 
 app = Flask(__name__)
 
-entries = []
+#entries = []
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    print([e for e in db.entries.find_one({})])
     if request.method == 'POST':
         entry_content = request.form.get("content")
         date_time = datetime.today()
         formatted_date = date_time.strftime("%d %b") ## 27 Dec
-        entries.append((entry_content, formatted_date))
-        db.entries.insert_one({"content": entry_content, "date":formatted_date })
-        print(entries)
-    return render_template("index.html", entries=entries)
+        #entries.append((entry_content,formatted_date))
+        db.entries.insert_one({"content":entry_content, "date":formatted_date})
+        #print(entries)
+        
+    entries_all = [
+        (
+            entry["content"],
+            entry["date"],
+        )
+        for entry in db.entries.find({})
+    ]
+    return render_template("index.html", entries=entries_all)
+        
+
+        
+
 
 @app.route("/calendar")
 def calendar():
