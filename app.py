fc5794a9865177be929984a88359ee4244c90b4f
juliocesar014 <juliocesar014@live.com>
2022-12-28 16:05:52 -0300

    pymongo
    feat/add pymongo and databases in app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, request
+from db import db
 
 app = Flask(__name__)
 
@@ -7,11 +8,13 @@ entries = []
 
 @app.route("/", methods=["GET","POST"])
 def home():
+    print([e for e in db.entries.find_one({})])
     if request.method == 'POST':
         entry_content = request.form.get("content")
         date_time = datetime.today()
         formatted_date = date_time.strftime("%d %b") ## 27 Dec
         entries.append((entry_content, formatted_date))
+        db.entries.insert_one({"content": entry_content, "date":formatted_date })
         print(entries)
     return render_template("index.html", entries=entries)
 
